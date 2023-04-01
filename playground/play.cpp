424ba82b181adb764d69597931affe27fc38fda4
Jecrevin <hrj18388@gmail.com>
2023-04-01 11:10:11 +0800

    tested machine precision of float & double

--- playground/play.cpp
+++ playground/play.cpp
@@ -2,19 +2,9 @@
 #include <limits>
 
 int main(int argc, char const *argv[]) {
-  std::cout << std::numeric_limits<bool>::min() << std::endl;
-  std::cout << std::numeric_limits<bool>::max() << std::endl;
-  std::cout << static_cast<int>(std::numeric_limits<unsigned char>::min())
-            << std::endl;
-  std::cout << static_cast<int>(std::numeric_limits<unsigned char>::max())
-            << std::endl;
-  std::cout << std::numeric_limits<int>::min() << std::endl;
-  std::cout << std::numeric_limits<int>::max() << std::endl;
-  std::cout << std::numeric_limits<unsigned int>::min() << std::endl;
-  std::cout << std::numeric_limits<unsigned int>::max() << std::endl;
-  std::cout << std::numeric_limits<long int>::min() << std::endl;
-  std::cout << std::numeric_limits<long int>::max() << std::endl;
-  std::cout << std::numeric_limits<long unsigned int>::min() << std::endl;
-  std::cout << std::numeric_limits<long unsigned int>::max() << std::endl;
+  std::cout << "float's machine precision: "
+            << std::numeric_limits<float>::min() << std::endl;
+  std::cout << "double's machine precision: "
+            << std::numeric_limits<double>::min() << std::endl;
   return 0;
 }
