cbb90ff94676ae1eb976df40b881517234a4fe57
Jecrevin <hrj18388@gmail.com>
2023-04-01 16:35:14 +0800

    do some more exercises in chapter 2

--- ch2/ex1.cpp
+++ ch2/ex1.cpp
@@ -1,12 +1,21 @@
 #include <complex>
 #include <iostream>
 
-typedef std::complex<double> Complex;
+typedef std::complex<double> complex;
+
+double squre(complex a, complex b, complex c) {
+  double h = (conj(b - a) * (c - a)).imag() / abs(b - a) / abs(c - a);
+  return h * abs(b - a);
+}
 
 int main(int argc, char const *argv[]) {
-  Complex A(3, 7), B(3, 2), C(10, 2);
-  double h = (conj(B - A) * (C - A)).imag() / abs(B - A) / abs(C - A);
-  double squre = h * abs(B - A);
-  std::cout << squre << std::endl;
+  complex a(3, 7), b(3, 2), c(10, 2);
+  double s = squre(a, b, c);
+  std::cout << s << std::endl;
+
+  complex t(4, 5);
+  complex a1 = a + t, b1 = b + t, c1 = c + t;
+  double s1 = squre(a1, b1, c1);
+  std::cout << s1 << std::endl;
   return 0;
 }
